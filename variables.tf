33e97bab6302c5c79d66e4b86cdea1d7366de7a6
Shivani Mehrotra <60307460+ShivaniMehrotra4@users.noreply.github.com>
2021-06-08 03:19:20 +0530

    Added variables file, though not needed now.

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "already_exists" {
+    type = string
+    default = "yes"
+    description = "This variable will define whether we want to create a new resource everytime the playbook is run, or not." 
+}
